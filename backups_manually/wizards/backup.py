9f7960379ef72e1461905e2da209af364f6f6143
oscars8a <oscar.soto.ochoa@gmail.com>
2019-06-13 15:49:37 +0200

    [FIX] a ver si traga

--- backups_manually/wizards/backup.py
+++ backups_manually/wizards/backup.py
@@ -1,3 +1,7 @@
+# © 2019 SDi Soluciones Informáticas
+# Author: Oscar Soto <osoto@sdi.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import http
 from odoo import api, fields, models
 
