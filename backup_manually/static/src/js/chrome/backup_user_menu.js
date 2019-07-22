172a7d80ba1dc8397e435097642b5002bfa69214
oscars8a <oscar.soto.ochoa@gmail.com>
2019-07-22 09:19:56 +0200

    [ADD] i18n [REF]clean code

--- backup_manually/static/src/js/chrome/backup_user_menu.js
+++ backup_manually/static/src/js/chrome/backup_user_menu.js
@@ -1,20 +1,11 @@
 odoo.define('backup_manually.UserMenu', function (require) {
     "use strict";
 
-    var Widget = require('web.Widget');
-    var SystrayMenu = require('web.SystrayMenu');
     var UserMenu = require('web.UserMenu');
-    var session = require('web.session');
-    var Dialog = require('web.Dialog');
-    var core = require('web.core');
-    var ajax = require('web.ajax');
-    var qweb = core.qweb;
-    var _t = core._t;
 
     UserMenu.include({
     _onMenuBackup: function () {
         var self = this;
-        var session = this.getSession();
         this.trigger_up('clear_uncommitted_changes', {
             callback: function () {
                 self._rpc({
@@ -24,12 +15,10 @@ odoo.define('backup_manually.UserMenu', function (require) {
                         },
                     })
                     .done(function (result) {
-                        result.res_id = session.uid;
                         self.do_action(result);
                     });
             },
         });
     },
     })
-
 })
